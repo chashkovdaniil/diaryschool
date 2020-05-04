fb3d28ffcbcd055f6d2280d7127fa0bc9a5b05c4
chashkovdaniil <chashkovdaniil@gmail.com>
2020-05-05 00:32:42 +0500

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class DiarySchoolApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Дневник',
+      title: 'Title',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.white),
       home: SafeArea(
