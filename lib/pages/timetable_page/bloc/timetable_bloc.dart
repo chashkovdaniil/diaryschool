d451eb126a7ff79f210a3eb9de1621d2828f433f
radomir9720 <ktbffh1905>
2020-05-20 12:28:26 +0300

    customTabBar не принимает состояния от bloc, только эвенты отправляет.

--- lib/pages/timetable_page/bloc/timetable_bloc.dart
+++ lib/pages/timetable_page/bloc/timetable_bloc.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:diaryschool/pages/timetable_page/bloc/timetable_event.dart';
-import 'package:diaryschool/pages/timetable_page/bloc/timetable_model.dart';
 import 'package:diaryschool/pages/timetable_page/bloc/timetable_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,43 +12,9 @@ class TimetableBloc extends Bloc<TimetableEvent, TimetableState> {
     TimetableEvent event,
   ) async* {
     if (event is WeekNavigationBarEvent) {
-      yield updateWeekNavigationBarState(event);
+      yield WeekNavigationBarState(
+          activeDay: event.activeDay,
+          firstDayOfCurrentWeek: event.firstDayOfCurrentWeek);
     }
-  }
-
-  WeekNavigationBarState updateWeekNavigationBarState(
-      WeekNavigationBarEvent event) {
-    /// Обновляет состояние WeekNavigationBatState.
-
-    DateTime firstDayOfCurrentWeek;
-
-    /// Если скролл скролл в правую сторону(для перемотки вперед)
-    // if (event.offset.toInt() >= event.maxScrollOffset.toInt()) {
-    //   /// Тут формируется состояние для иконки вперед
-
-    //   /// [firstDayOfCurrentWeek] - Тут проверяются условия для перемотки на одну неделю вперед
-    //   if (event.maxScrollOffset.toInt() == event.offset.toInt() &&
-    //       event.wasActivated == IconType.next) {
-    //     firstDayOfCurrentWeek =
-    //         event.firstDayOfCurrentWeek.add(const Duration(days: 7));
-    //   }
-    // }
-
-    /// Если скролл в левую сторону(для переметки назад)
-    // if (event.offset.toInt() <= 0) {
-  
-
-    //   /// [firstDayOfCurrentWeek] - Тут проверяются условия для перемотки на одну неделю назад
-    //   if (event.offset.toInt() == 0 &&
-    //       event.wasActivated == IconType.previous) {
-    //     firstDayOfCurrentWeek =
-    //         event.firstDayOfCurrentWeek.subtract(const Duration(days: 7));
-    //   }
-
-    // }
-    return WeekNavigationBarState(
-        activeDay: event.activeDay,
-        firstDayOfCurrentWeek:
-            firstDayOfCurrentWeek ?? event.firstDayOfCurrentWeek);
   }
 }
